c35f3af2e9dda29e453cfaeb2905b11091629f72
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-01-22 16:18:29 -0500

    Including apollo client to main file

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,28 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
 import { createPinia } from 'pinia'
+
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
 
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const cache = new InMemoryCache()
+const apolloClient = new ApolloClient({
+  cache,
+  uri: 'https://rickandmortyapi.com/graphql',
+  
+})
+
+const app = createApp({
+  setup () {
+    provide(DefaultApolloClient, apolloClient)
+  },
+
+  render: () => h(App),
+})
 
 app.use(createPinia())
 app.use(router)
