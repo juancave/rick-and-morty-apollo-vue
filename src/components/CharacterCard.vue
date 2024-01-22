06bd852ca6d18e8626a0b1fff9a59977d140a62f
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-01-22 15:41:14 -0500

    Adding Character card using mock data

---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+defineProps<{
+  id: string
+  name: string
+  status: string
+  species: string
+  gender: string
+}>()
+</script>
+
+<template>
+  <article class="card">
+    <div class="image-wrapper">
+      <img :src="'https://rickandmortyapi.com/api/character/avatar/' + id + '.jpeg'" :alt="name">
+    </div>
+    <div class="content">
+      <div class="section">
+        <h2 class="green">{{ name }}</h2>
+      </div>
+      <hr>
+      <div class="section">
+        <span class="bold">Status: </span><span class="status"><span class="status-icon"></span>{{ status }} {{ species }}</span>
+      </div>
+      <div class="section">
+        <span class="bold">Gender: </span><span class="gender">{{ gender }}</span>
+      </div>
+    </div>
+  </article>
+</template>
+
+<style scoped>
+h2 {
+  font-weight: bold;
+}
+</style>
--- src/views/CharactersView.vue
+++ src/views/CharactersView.vue
@@ -1,9 +1,81 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import CharacterCard from '../components/CharacterCard.vue'
+
+const mockData = [
+  {
+    "id": "1",
+    "name": "Rick Sanchez",
+    "status": "Alive",
+    "species": "Human",
+    "gender": "Male"
+  },
+  {
+    "id": "2",
+    "name": "Morty Smith",
+    "status": "Alive",
+    "species": "Human",
+    "gender": "Male"
+  },
+  {
+    "id": "3",
+    "name": "Summer Smith",
+    "status": "Alive",
+    "species": "Human",
+    "gender": "Female"
+  },
+  {
+    "id": "4",
+    "name": "Beth Smith",
+    "status": "Alive",
+    "species": "Human",
+    "gender": "Female"
+  },
+  {
+    "id": "5",
+    "name": "Jerry Smith",
+    "status": "Alive",
+    "species": "Human",
+    "gender": "Male"
+  },
+  {
+    "id": "6",
+    "name": "Abadango Cluster Princess",
+    "status": "Alive",
+    "species": "Alien",
+    "gender": "Female"
+  },
+  {
+    "id": "7",
+    "name": "Abradolf Lincler",
+    "status": "unknown",
+    "species": "Human",
+    "gender": "Male"
+  },
+  {
+    "id": "8",
+    "name": "Adjudicator Rick",
+    "status": "Dead",
+    "species": "Human",
+    "gender": "Male"
+  }
+];
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <template v-for="item in mockData">
+      <CharacterCard
+        :name="item.name" :gender="item.gender" :id="item.id" :status="item.status" :species="item.species" />
+    </template>
+  </div>
 </template>
+
+<style scoped>
+  .container {
+    display: flex;
+    justify-content: center;
+    gap: 2rem;
+    flex-wrap: wrap;
+  }
+</style>
